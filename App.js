1a4a12a9ee8684edf8ad01f3ac93bbbd1ddf8b9a
2ez4p4ul <allan@gmail.com>
2025-09-20 03:12:55 +0800

    Add Counter and Color

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
-import CommentSection from './CommentSection';
-import ChatBubble from './ChatBubble';
-
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
 export default function App() {
   return (
     <View style={styles.container}>
-      <CommentSection/>
-      <ChatBubble/>
+      <CounterApp/>
+      <ColorChangerApp/>
     </View>
   );
 }
