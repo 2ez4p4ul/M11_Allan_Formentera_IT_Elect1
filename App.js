9bdec2fc9baf95b06595c9df4510eb302ce1397d
2ez4p4ul <allan@gmail.com>
2025-09-20 03:18:47 +0800

    Comment and Chat

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
+
 import CommentSection from './CommentSection';
 import ChatBubble from './ChatBubble';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CommentSection/>
+ <CommentSection/>
       <ChatBubble/>
     </View>
   );
